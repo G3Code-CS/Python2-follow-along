a94d740faf2502cc878280b350460405244bad1c
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-11-18 19:01:27 -0600

    Completed Created, select and quit for todo list

--- todoList.py
+++ todoList.py
@@ -11,10 +11,11 @@ class TodoList:
 
 quit = False
 all_lists = []
+current_list = None
 
 while not quit:
     #Get the input from the user
-    command = input("(C)reate a new list\n(S)elect a list\n(A)dd an item\n(Q)uit\nCommand : ")
+    command = input(f"(C)reate a new list\n(S)elect a list ({all_lists})\n(A)dd an item\n(Q)uit\nCommand : ")
     
     command = command.lower().strip()[0]
     if command == 'q': # quit
@@ -27,3 +28,18 @@ while not quit:
         all_lists.append(new_list)
         print(all_lists)
 
+    elif command == 's': # Select
+        name = input("Enter list name:").strip()
+        named_list = None
+
+        for l in all_lists:
+            if l.name == name:
+                named_list = l
+                break
+        
+        if named_list is None:
+            print(f"No such list named {name}")
+
+        else:
+            current_list = named_list
+            print(f"Current selected list is ({current_list})")
